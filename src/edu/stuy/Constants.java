569794b855c17c8e9e1d0ddca1f93efa7c4e1c9f
Abdullah Fahad <abdullah.fahad.14@gmail.com>
2014-01-13 17:42:21 -0500

    Create basic Acquirer.

--- src/edu/stuy/Constants.java
+++ src/edu/stuy/Constants.java
@@ -12,8 +12,15 @@ package edu.stuy;
  */
 public class Constants {
     
+    /* PWM */
     public static final int LEFT_MOTOR_CHANNEL = 1;
     public static final int RIGHT_MOTOR_CHANNEL = 2;
-
+    public static final int ACQUIRER_ROLLER_CHANNEL = 3;
     
+    /* SOLENOID */
+    public static final int THING_DOWN_CHANNEL = 1;
+    public static final int THING_UP_CHANNEL = 2;
+
+    /* GPIO */
+    public static final int BALL_SENSOR_CHANNEL = 1;
 }
--- src/edu/stuy/subsystems/Acquirer.java
+++ src/edu/stuy/subsystems/Acquirer.java
@@ -6,10 +6,67 @@
 
 package edu.stuy.subsystems;
 
+import edu.stuy.Constants;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+
 /**
  *
  * @author abdullahfahad
  */
 public class Acquirer {
+    
+    private static Acquirer instance;
+    private Solenoid thingDown;
+    private Solenoid thingUp;
+    private Talon roller;
+    private DigitalInput ballSensor;
+ 
+    public Acquirer() {
+        thingDown = new Solenoid(Constants.THING_DOWN_CHANNEL);
+        thingUp = new Solenoid(Constants.THING_UP_CHANNEL);
+        roller = new Talon(Constants.ACQUIRER_ROLLER_CHANNEL);
+        ballSensor = new DigitalInput(Constants.BALL_SENSOR_CHANNEL);
+    }
+    
+    public Acquirer getInstance() {
+        if (instance == null) {
+            instance = new Acquirer();
+        }
+        return instance;
+    }
+    
+    public void rotateDown() {
+        thingDown.set(true);
+        thingUp.set(false);
+    }
+    
+    public void rotateUp() {
+        thingDown.set(false);
+        thingUp.set(true);
+    }
+    
+    public void intakeBall() {
+        roller.set(-1);
+    }
+    
+    public void ejectBall() {
+        roller.set(1);
+    }
+    
+    public void stopRoller() {
+        roller.set(0);
+    }
+    
+    public void reset() {
+        rotateUp();
+        stopRoller();
+    }
+    
+    public boolean hasBall() {
+        return ballSensor.get();
+    }
+    
     
 }
