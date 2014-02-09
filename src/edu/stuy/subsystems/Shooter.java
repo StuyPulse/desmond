1813e1408f481e856ce0d1925d3d3d5ca6d65fbf
Shriken <sterling.zane@gmail.com>
2014-02-09 12:27:00 -0500

    Add retracting logic for Shooter

--- src/edu/stuy/subsystems/Shooter.java
+++ src/edu/stuy/subsystems/Shooter.java
@@ -1,6 +1,7 @@
 package edu.stuy.subsystems;
 
 import edu.stuy.Constants;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.stuy.util.Gamepad;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -8,118 +9,118 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 
 public class Shooter {
-    public static final double POSITION_ONE = 0.1;
-    public static final double POSITION_TWO = 0.0;
-    
-     private AnalogChannel goalSensor;
-     private Talon chooChoo;
-     //private Encoder winchEncoder;
-     private static Shooter instance;
-     private DigitalInput ballSensor;
-     private DigitalInput ballCenteredSwitch;
-     private DigitalInput catapultRetractedSwitch;
 
-     
-     private Shooter() {
-         goalSensor = new AnalogChannel(Constants.GOAL_SENSOR_CHANNEL);
-         chooChoo = new Talon(Constants.SHOOTER_CHANNEL);
-         /*winchEncoder = new Encoder(Constants.WINCH_ENCODER_CHANNEL_A, Constants.WINCH_ENCODER_CHANNEL_B);
+    private boolean retracting = false;
+    private AnalogChannel goalSensor;
+    private Talon chooChoo;
+    //private Encoder winchEncoder;
+    private static Shooter instance;
+    private DigitalInput ballSensor;
+    private DigitalInput ballCenteredSwitch;
+    private DigitalInput catapultRetractedSwitch;
+    private long startTime = System.currentTimeMillis();
+
+    private Shooter() {
+        goalSensor = new AnalogChannel(Constants.GOAL_SENSOR_CHANNEL);
+        chooChoo = new Talon(Constants.SHOOTER_CHANNEL);
+        /*winchEncoder = new Encoder(Constants.WINCH_ENCODER_CHANNEL_A, Constants.WINCH_ENCODER_CHANNEL_B);
          winchEncoder.start();
          winchEncoder.reset(); */
-         ballSensor = new DigitalInput(Constants.BALL_SENSOR_CHANNEL);
-         ballCenteredSwitch = new DigitalInput(Constants.BALL_CENTERED_SWITCH_CHANNEL);
-         catapultRetractedSwitch = new DigitalInput(Constants.CATAPULT_RETRACTED_SWITCH_CHANNEL);
-     }
-     
-     public static Shooter getInstance() {
+        ballSensor = new DigitalInput(Constants.BALL_SENSOR_CHANNEL);
+        ballCenteredSwitch = new DigitalInput(Constants.BALL_CENTERED_SWITCH_CHANNEL);
+        catapultRetractedSwitch = new DigitalInput(Constants.CATAPULT_RETRACTED_SWITCH_CHANNEL);
+    }
+
+    public static Shooter getInstance() {
         if (instance == null) {
             instance = new Shooter();
-        }   
+        }
         return instance;
-     }
-     
-     public void reset() {
-         retractWinch();
-     }
-     
-     public void releaseWinch() {
-         if (readyToShoot()) {
-            //int angle = getAngle();
-            //if(angle <= (Constants.DEGREES_WINCH_RELEASE/2)) {
-            chooChoo.set(Constants.SHOOTER_WINCH_SPEED_ONE);
-         } else {
-            chooChoo.set(Constants.SHOOTER_WINCH_SPEED_TWO);
-         }
-     }
-     
-     public void retractWinch() {
-         //int angle = getAngle();
-         //if (angle >= Constants.DEGREES_WINCH_RETRACT/2) {
-         if(isFullyRetracted()){
-            chooChoo.set(Constants.SHOOTER_WINCH_SPEED_ONE);
-         } else {
-            chooChoo.set(Constants.SHOOTER_WINCH_SPEED_TWO);
-         }
-     }
-     public void stopWinch() {
-         chooChoo.set(0);
-     }
-     /*
+    }
+
+    public void reset() {
+        retractWinch();
+    }
+
+    public void releaseWinch() {
+        if (isFullyRetracted() && !retracting) {
+            chooChoo.set(1.0);
+            Timer.delay(Constants.SHOOTER_RETRACT_DELAY);
+            chooChoo.set(0.0);
+        }
+    }
+
+    public void retractWinch() {
+        if (retracting && !isFullyRetracted() && (System.currentTimeMillis() - startTime) < Constants.SHOOTER_RETRACT_TIMEOUT) {
+            chooChoo.set(1.0);
+        }
+        else {
+            chooChoo.set(0.0);
+            retracting = false;
+        }
+    }
+
+    public void initiateWinch() {
+        retracting = true;
+        startTime = System.currentTimeMillis();
+    } 
+    
+    public void stopWinch() {
+        chooChoo.set(0);
+        retracting = false;
+    }
+    /*
      public int getAngle() {
-         double pulses = winchEncoder.get() % Constants.PULSES_PER_REVOLUTION;
-         double angle = pulses / Constants.PULSES_PER_REVOLUTION * 360;
-         return (int) angle;
+     double pulses = winchEncoder.get() % Constants.PULSES_PER_REVOLUTION;
+     double angle = pulses / Constants.PULSES_PER_REVOLUTION * 360;
+     return (int) angle;
      }
      */
-     
-     public boolean hasBall() {
-         return ballSensor.get();
-     }
-     
-     public boolean isBallCentered() {
-         return !ballCenteredSwitch.get(); //closed switch is false
-     }
 
-     public boolean isFullyRetracted() {
-         return !catapultRetractedSwitch.get(); //closed switch is false
-     }
-     
-     public boolean readyToShoot() {
-         /*int angle = getAngle();
+    public boolean hasBall() {
+        return ballSensor.get();
+    }
+
+    public boolean isBallCentered() {
+        return !ballCenteredSwitch.get(); //closed switch is false
+    }
+
+    public boolean isFullyRetracted() {
+        return !catapultRetractedSwitch.get(); //closed switch is false
+    }
+
+    public boolean readyToShoot() {
+        /*int angle = getAngle();
          return (angle < 340 && angle > 380-Constants.DEGREES_WINCH_RETRACT);
          */
-         return isFullyRetracted();
-     }
+        return isFullyRetracted();
+    }
 
-     public boolean isGoalHot() {
-         double voltage = goalSensor.getAverageVoltage();
-         return (voltage >= Constants.SHOOTER_GOAL_SENSOR_VOLTAGE);
-     }
-     
-     public void manualGamepadControl(Gamepad gamepad) {
-         if (gamepad.getRightBumper()) {
-             releaseWinch();
-         }
-         else if (gamepad.getStartButton()) {
-             stopWinch();
-         }
-         else if (gamepad.getLeftBumper()) {
-             retractWinch();
-         }
-         else if (gamepad.getLeftY() > 0){
-             chooChoo.set(gamepad.getLeftY());
-         }
-         else if (gamepad.getLeftY() <= 0){
-             chooChoo.set(0);
-         }
-     }
-     
-     public void testChooChoo(Gamepad gamepad){
-         if (gamepad.getLeftButton()){
-             chooChoo.set(1);
-         }
-         else if (gamepad.getBottomButton()){
-             chooChoo.set(0);
-         }
-     }
+    public boolean isGoalHot() {
+        double voltage = goalSensor.getAverageVoltage();
+        return (voltage >= Constants.SHOOTER_GOAL_SENSOR_VOLTAGE);
+    }
+
+    public void manualGamepadControl(Gamepad gamepad) {
+        if (gamepad.getRightBumper()) {
+            releaseWinch();
+        } else if (gamepad.getStartButton()) {
+            stopWinch();
+        } else if (gamepad.getLeftBumper()) {
+            initiateWinch();
+        } else if (gamepad.getLeftY() > 0) {
+            chooChoo.set(gamepad.getLeftY());
+        } else if (gamepad.getLeftY() <= 0) {
+            chooChoo.set(0);
+        }
+        retractWinch();
+    }
+
+    public void testChooChoo(Gamepad gamepad) {
+        if (gamepad.getLeftButton()) {
+            chooChoo.set(1);
+        } else if (gamepad.getBottomButton()) {
+            chooChoo.set(0);
+        }
+    }
 }
